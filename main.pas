01c80f2fb3f7e29210c56f5ed70dbb8c5b2267a9
Nina Sepúlveda Soares Carvalho <Dev10@HEARTWARE.local>
2026-05-12 13:44:28 -0300

    second commit

--- main.pas
+++ main.pas
@@ -16,68 +16,101 @@ var
   arrayNumeros : array[1..5] of integer;
   matrizNumeros : array[1..3, 1..5] of integer;
 
+
+procedure sayHello();
 begin
-  write('Raio do circulo: ');
-  readln(r);
+  writeln('Hello, World!');
+end;
 
-  d := r * 2;
-  c := d * PI;
+procedure say(userName, favColor : String;age : integer);
+begin
+  writeln('Hello, ', userName, '!');
+  writeln('Your favorite color is ', favColor, ' and you are ', age, ' years old.');  
+end;
 
-  writeln('Raio = ', r:0:2,
-          ' Diametro = ', d:0:2,
-          ' Comprimento = ', c:7:2);
+procedure soma(num1, num2: integer);
+var
+  resultado: integer;
+begin
+  resultado := num1 + num2;
+  writeln('Soma: ', resultado);
+end;
 
-  contador := 1;
+function funcSoma(a,b : integer):integer;
+var 
+  resultado: integer;
+begin
+  resultado := a + b;
+  funcSoma := resultado;
+end;
 
-  while contador <= 20 do
-  begin
-    if (contador mod 3 = 0) and (contador mod 5 = 0) then
-      writeln('FizzBuzz: ', contador)
-    else if (contador mod 3 = 0) then
-      writeln('Fizz: ', contador)
-    else if (contador mod 5 = 0) then
-      writeln('Buzz: ', contador);
+begin
+  sayHello();
+  say('Alice', 'blue', 30);
+  soma(5, 10);
+  writeln('Soma: ', funcSoma(7, 3));
 
-    contador := contador + 1;
-  end;
+//   write('Raio do circulo: ');
+//   readln(r);
 
-  n1 := 10;
+//   d := r * 2;
+//   c := d * PI;
 
-  case n1 of
-    10:
-    begin
-      writeln('n1 igual a 10');
-    end;
-    20:
-    begin
-      writeln('n1 igual a 20');
-    end;
-  end;
+//   writeln('Raio = ', r:0:2,
+//           ' Diametro = ', d:0:2,
+//           ' Comprimento = ', c:7:2);
 
-  for i := 1 to 10 do
-  begin
-    writeln('Contador For: ', i);
-  end;
+//   contador := 1;
 
-  arrayNumeros[1] := 5;
-  arrayNumeros[2] := 10;
-  arrayNumeros[3] := 15;
-  arrayNumeros[4] := 20;
-  arrayNumeros[5] := 25;
+//   while contador <= 20 do
+//   begin
+//     if (contador mod 3 = 0) and (contador mod 5 = 0) then
+//       writeln('FizzBuzz: ', contador)
+//     else if (contador mod 3 = 0) then
+//       writeln('Fizz: ', contador)
+//     else if (contador mod 5 = 0) then
+//       writeln('Buzz: ', contador);
 
-  for i := 1 to length(arrayNumeros) do
-  begin
-    writeln('Array Numeros: ', arrayNumeros[i]);
-  end;
+//     contador := contador + 1;
+//   end;
 
- for i := 1 to 3 do
- begin
-   for j := 1 to 5 do
-   begin
-    matrizNumeros[i, j] := i * j; 
-    writeln('Matriz Numeros: ', matrizNumeros[i, j]);
-   end;
- end;
+//   n1 := 10;
+
+//   case n1 of
+//     10:
+//     begin
+//       writeln('n1 igual a 10');
+//     end;
+//     20:
+//     begin
+//       writeln('n1 igual a 20');
+//     end;
+//   end;
+
+//   for i := 1 to 10 do
+//   begin
+//     writeln('Contador For: ', i);
+//   end;
+
+//   arrayNumeros[1] := 5;
+//   arrayNumeros[2] := 10;
+//   arrayNumeros[3] := 15;
+//   arrayNumeros[4] := 20;
+//   arrayNumeros[5] := 25;
+
+//   for i := 1 to length(arrayNumeros) do
+//   begin
+//     writeln('Array Numeros: ', arrayNumeros[i]);
+//   end;
+
+//  for i := 1 to 3 do
+//  begin
+//    for j := 1 to 5 do
+//    begin
+//     matrizNumeros[i, j] := i * j; 
+//     writeln('Matriz Numeros: ', matrizNumeros[i, j]);
+//    end;
+//  end;
 
  readln;
 end.
